5719ec5a25d842a57f7e42e9f839428146ceadfb
Umar Ahmad <ahmad.umar2008@yahoo.in>
2014-04-19 15:15:17 -0400

    tst

--- gsfhack_flaskapp/app/views.py
+++ gsfhack_flaskapp/app/views.py
@@ -2,13 +2,11 @@ import time
 import os
 from flask import Blueprint, request, jsonify
 from werkzeug import secure_filename
-from flask.ext.babel import gettext
 from app import app
 import kookoo
 import pickle
 import sys, os, json, urllib2, urllib, time
-
-
+import requests
 @app.route('/')
 @app.route('/index')
 @app.route('/api/', methods=['GET'])
@@ -26,16 +24,25 @@ USERNAME = "hammadhaleem@gmail.com"
 PASSWORD = "9H398339966MFP6"
 
 def transcribe_audio_file(fi):
-    url = 'https://api.nexiwave.com/SpeechIndexing/file/storage/' + USERNAME +'/recording/?authData.passwd=' + PASSWORD + '&auto-redirect=true&response=application/json'
-    r = requests.post(url, files={'mediaFileData': fi})
-    data = r.json()
-    transcript = data['text']
-    return  "Transcript for "+filename+"=" + transcript
+    
+    try:
+	url = 'https://api.nexiwave.com/SpeechIndexing/file/storage/' + USERNAME +'/recording/?authData.passwd=' + PASSWORD + '&auto-redirect=true&response=application/json'
+    	r = requests.post(url, files={'mediaFileData': fi})
+   	data = r.json()
+    	transcript = data['text']
+    	return  "Transcript for "+filename+"=" + transcript
+    except :
+	return 0
 
 def getfile(url):
-	response = urllib2.urlopen(url)
-	html = response.read()
-	return html
+	#url ="http://recordings.kookoo.in/nayyar_vipul/name1397932661.58.wav"
+	try :
+		response = urllib2.urlopen(url)
+		html = response.read()
+		return html
+	except :
+		print url
+		return 0
 
 @app.route('/api/kookoo/process', methods=['GET','POST'])
 @app.route('/api/kookoo/process/', methods=['GET','POST'])
@@ -45,11 +52,11 @@ def ProcessSong():
 	for i,j in lis.items():
 		stri = stri + "<li><a href='/api/kookoo/process/?no="+str(i)+"'>"+str(i)+"</a></li>"
 	if request.values.get("no"):
-		dici = lis[request.values.get("no")]
+		dici = lis[str(request.values.get("no"))]
 		for k,m in dici.items():
-			var  ="http://recordings.kookoo.in/nayyar_vipul/"+m+".wav"
-			dici[k]=transcribe_audio_file(getfile(var))
-			
+			var  ="http://recordings.kookoo.in/nayyar_vipul/"+str(m)+".wav"
+			#return var 
+			dici[k]= transcribe_audio_file(getfile(var))
 		return jsonify(dici)
 	else:
 		return stri+"</ul>"
@@ -66,10 +73,10 @@ def ProcessKooKooResponse():
     curtime = str(time.time())
     dic ={'Please say your name':'name'+curtime,
           'Please Say your qualification':'qual'+curtime}
-    phone_number=request.values.get("called_number")
+    phone_number=request.values.get("cid")
     if request.values.get("event") == "NewCall":
         r.addPlayText("Welcome to Shine dot com registration service")
-        phone_number=request.values.get("called_number")
+        phone_number=request.values.get("cid")
 	for i , j in dic.items():
         	r.addPlayText(i)
         	r.addRecord(j)
--- save.p
+++ save.p
@@ -4,13 +4,27 @@ p1
 (dp2
 S'Please say your name'
 p3
-S'name1397932307.64'
+S'name1397933197.56'
 p4
 sS'Please Say your qualification'
 p5
-S'qual1397932307.64'
+S'qual1397933197.56'
 p6
 sS'proc'
 p7
+I0
+ssV1126924703
+p8
+(dp9
+S'Please say your name'
+p10
+S'name1397934731.93'
+p11
+sS'Please Say your qualification'
+p12
+S'qual1397934731.93'
+p13
+sS'proc'
+p14
 I0
 ss.
