72f97df7069d5f824797a026ed4a57b917100fbe
Umar Ahmad <ahmad.umar2008@yahoo.in>
2014-04-19 14:34:41 -0400

    added processing

--- gsfhack_flaskapp/app/views.py
+++ gsfhack_flaskapp/app/views.py
@@ -6,15 +6,11 @@ from flask.ext.babel import gettext
 from app import app
 import kookoo
 import pickle
+import sys, os, json, urllib2, urllib, time
 
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return "Hello, World!"
-
-
-
 @app.route('/api/', methods=['GET'])
 @app.route('/api', methods=['GET'])
 def index():
@@ -25,6 +21,39 @@ def index():
     return jsonify(items=json_results)
 
 
+
+USERNAME = "hammadhaleem@gmail.com"
+PASSWORD = "9H398339966MFP6"
+
+def transcribe_audio_file(fi):
+    url = 'https://api.nexiwave.com/SpeechIndexing/file/storage/' + USERNAME +'/recording/?authData.passwd=' + PASSWORD + '&auto-redirect=true&response=application/json'
+    r = requests.post(url, files={'mediaFileData': fi})
+    data = r.json()
+    transcript = data['text']
+    return  "Transcript for "+filename+"=" + transcript
+
+def getfile(url):
+	response = urllib2.urlopen(url)
+	html = response.read()
+	return html
+
+@app.route('/api/kookoo/process', methods=['GET','POST'])
+@app.route('/api/kookoo/process/', methods=['GET','POST'])
+def ProcessSong():
+	lis = pickle.load( open( "save.p", "rb" ) )
+	stri = "<ul>"
+	for i,j in lis.items():
+		stri = stri + "<li><a href='/api/kookoo/process/?no="+str(i)+"'>"+str(i)+"</a></li>"
+	if request.values.get("no"):
+		dici = lis[request.values.get("no")]
+		for k,m in dici.items():
+			var  ="http://recordings.kookoo.in/nayyar_vipul/"+m+".wav"
+			dici[k]=transcribe_audio_file(getfile(var))
+			
+		return jsonify(dici)
+	else:
+		return stri+"</ul>"
+		
 
 @app.route('/api/kookoo', methods=['GET','POST'])
 @app.route('/api/kookoo/', methods=['GET','POST'])
@@ -37,7 +66,7 @@ def ProcessKooKooResponse():
     curtime = str(time.time())
     dic ={'Please say your name':'name'+curtime,
           'Please Say your qualification':'qual'+curtime}
-    
+    phone_number=request.values.get("called_number")
     if request.values.get("event") == "NewCall":
         r.addPlayText("Welcome to Shine dot com registration service")
         phone_number=request.values.get("called_number")
@@ -46,6 +75,7 @@ def ProcessKooKooResponse():
         	r.addRecord(j)
         r.addPlayText("Thank You")
         r.addHangup()
+    dic['proc'] = 0
     lis[phone_number]=dic
     pickle.dump( lis, open( "save.p", "wb" ) )
     return str(r).replace("<Record","<Record format=\"wav\" silence=\"3\" maxduration=\"5\"")
@@ -54,24 +84,22 @@ def ProcessKooKooResponse():
 @app.route('/api/upload', methods=['POST'])
 def upload():
     if request.method == 'POST':
-        upload_folder = os.getcwd()+"/uploads/"
+        upload_folder = os.path.join(os.getcwd(),'/uploads/')
         if not os.path.exists(upload_folder):
             os.mkdir(upload_folder)
-            print upload_folder
+            return upload_folder
         files = []
         f = request.files['file']
         if f:
             filename = secure_filename(f.filename)
-	    m = os.path.join(upload_folder, filename)
-            f.save(m)
+            f.save(os.path.join(upload_folder, filename))
         output = {
-            'name': filename+m,
+            'name': filename,
             'status': 'success',
-	    'url' : 'http://107.161.27.22/gsfhack/gsfhack_flaskapp/app/uploads/'+str(filename)
+	    'url' : 'http://107.161.27.22/gsfhack/uploads/'+str(filename)
         }
         files.append(output)
     return jsonify(files=files)
-
 @app.route('/api/test', methods=['GET'])
 @app.route('/api/test/', methods=['GET'])
 def testform():
--- save.p
+++ save.p
@@ -1,13 +1,16 @@
 (dp0
-V43243463
+V911130715373
 p1
 (dp2
 S'Please say your name'
 p3
-S'name1397929560.2'
+S'name1397932307.64'
 p4
 sS'Please Say your qualification'
 p5
-S'qual1397929560.2'
+S'qual1397932307.64'
 p6
+sS'proc'
+p7
+I0
 ss.
